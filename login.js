f852fa3fe3177d9e075c5ab31d15f45c6b2cb905
Shivanibhadoriya <shivanibhadouriya2003@gmail.com>
2024-08-06 18:53:00 +0530

    authentication added

--- login.js
+++ login.js
@@ -19,11 +19,11 @@ function checkData() {
   if (email == "" || password == "") {
     let i = 0;
 
-    if (email == "") {
+    if (!email.endsWith("@gmail.com")) {
       i++;  // I am incrementing 'i' just because invalid password  field can add on right place
       let child = document.createElement("p");
       child.classList.add('empty-text');
-      child.innerHTML = '<i class="fa-solid fa-circle-exclamation" style="color: #c20000;"></i>Enter your mobile number or address';
+      child.innerHTML = '<i class="fa-solid fa-circle-exclamation" style="color: #c20000;"></i>' + (email ==""? "Enter your mobile number or address":"wrong EmailId");
       parent.insertBefore(child, parent.children[2]);
 
     }
@@ -41,26 +41,30 @@ function checkData() {
     // JSON.parse -> convert string into  Object  ,
     user_records = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [];
 
-    // v is iterator for iterating each element of array
+   // v is iterator for iterating each element of array
     if (user_records.some((v) => {
-      return v.email == email && v.password == password;
+      return v.email == email ;
     })) {
       let current_user = user_records.filter((v) => {
         return v.email == email && v.password == password;
-      })[0];
-      
-      window.location.href = 'index.html?authenticated=true';
-      console.log("hello I have done");
-      localStorage.setItem("email",current_user.email);
-      
+      });
+      if(current_user.length == 1){
+        window.location.href = 'index.html?authenticated=true';
+        console.log("hello I have done");
+        localStorage.setItem("email",current_user[0].email);  
+      }else{
+        
+        let child = document.createElement("p");
+        child.classList.add('empty-password');
+        child.innerHTML = '<i class="fa-solid fa-circle-exclamation" style="color: #c20000;"></i>Invalid Email or Password';
+        parent.insertBefore(child, parent.children[4]);  
+      }
     }
     else {
-
       let child = document.createElement("p");
       child.classList.add('empty-password');
-      child.innerHTML = '<i class="fa-solid fa-circle-exclamation" style="color: #c20000;"></i>Invalid Email or Password';
-      parent.insertBefore(child, parent.children[4]);
-
+      child.innerHTML = '<i class="fa-solid fa-circle-exclamation" style="color: #c20000;"></i>User doesn\'t exit';
+      parent.insertBefore(child, parent.children[4]);  
     }
   }
 }
